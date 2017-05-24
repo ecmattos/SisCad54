7895000d3a39a1746d3f6706db8ed5f3a751495b
eduardo <ecmattos@ig.com.br>
2017-05-24 15:28:18 -0300

    PatrimonialBrands - Cabecalho botao voltar

--- resources/views/patrimonial_brands/edit.blade.php
+++ resources/views/patrimonial_brands/edit.blade.php
@@ -5,11 +5,9 @@
 	<ol class="breadcrumb">
   		<li class="breadcrumb-item">Administração</li>
   		<li class="breadcrumb-item">Ativos</li>
-  		<li class="breadcrumb-item"><a href="{!! route('patrimonial_brands') !!}">Marcas</a></li>
+  		<li class="breadcrumb-item"><a href="{!! route('patrimonial_brands') !!}" class="btn btn-sm btn-warning"><i class="fa fa-arrow-left"></i> <b>Marcas</b></a></li>
   		<li class="breadcrumb-item"><b>Alteração</b></li>
 	</ol>
-
-	<hr class="hr-warning" />
 
 	{!! Form::model($patrimonial_brand, ['route' => ['patrimonial_brands.update', $patrimonial_brand->id], 'method' => 'put', 'class' => 'form-horizontal', 'role'=>'form']) !!}
 
