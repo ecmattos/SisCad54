ff1b8fc22f201afc1afb387aa011b09b7b7a89ec
eduardo <ecmattos@ig.com.br>
2017-05-24 10:57:39 -0300

    Meetings - Cabecalho botao voltar

--- resources/views/meetings/create.blade.php
+++ resources/views/meetings/create.blade.php
@@ -3,11 +3,9 @@
 @section('content')
 	
 	<ol class="breadcrumb">
-  		<li class="breadcrumb-item"><a href="{!! route('meetings') !!}">Eventos</a></li>
-  		<li class="breadcrumb-item"><b>Inclusão</b></li>
+  		<li class="breadcrumb-item"><a href="{!! route('meetings') !!}" class="btn btn-sm btn-warning"><i class="fa fa-arrow-left"></i> <b>Eventos</b></a></li>
+		<li class="breadcrumb-item"><b>Inclusão</b></li>
 	</ol>
-
-	<hr class="hr-warning" />
 
 	{!! Form::open(['route' => 'meetings.store', 'class'=>'form-horizontal', 'role'=>'form']) !!}
 
--- resources/views/meetings/edit.blade.php
+++ resources/views/meetings/edit.blade.php
@@ -3,11 +3,9 @@
 @section('content')
 
 	<ol class="breadcrumb">
-  		<li class="breadcrumb-item"><a href="{!! route('meetings') !!}">Eventos</a></li>
+  		<li class="breadcrumb-item"><a href="{!! route('meetings') !!}" class="btn btn-sm btn-warning"><i class="fa fa-arrow-left"></i> <b>Eventos</b></a></li>
   		<li class="breadcrumb-item"><b>Alteração</b></li>
 	</ol>
-
-	<hr class="hr-warning" />
 
 	{!! Form::model($meeting, ['route' => ['meetings.update', $meeting->id], 'method' => 'put', 'class' => 'form-horizontal', 'role'=>'form']) !!}
 
