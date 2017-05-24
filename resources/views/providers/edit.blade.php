0a710851669f83b2614e4b954b04e845af663a8c
eduardo <ecmattos@ig.com.br>
2017-05-24 11:06:11 -0300

    Providers - Cabecalho botao voltar

--- resources/views/providers/edit.blade.php
+++ resources/views/providers/edit.blade.php
@@ -3,11 +3,9 @@
 @section('content')
 
 	<ol class="breadcrumb">
-  		<li class="breadcrumb-item"><a href="{!! route('providers') !!}">Fonecedores</a></li>
+  		<li class="breadcrumb-item"><a href="{!! route('providers') !!}" class="btn btn-sm btn-warning"><i class="fa fa-arrow-left"></i> <b>Fornecedores</b></a></li>
   		<li class="breadcrumb-item"><b>Alteração</b></li>
 	</ol>
-
-	<hr class="hr-warning" />
 
 	{!! Form::model($provider, ['route' => ['providers.update', $provider->id], 'method' => 'put', 'class' => 'form-horizontal', 'role'=>'form']) !!}
 
--- resources/views/providers/show.blade.php
+++ resources/views/providers/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 	<ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="{!! route('providers') !!}">Fornecedores</a></li>
+    <li class="breadcrumb-item"><a href="{!! route('providers') !!}" class="btn btn-sm btn-warning"><i class="fa fa-arrow-left"></i> <b>Fornecedores</b></a></li>
     <li class="breadcrumb-item"><b>Consulta</b></li>
 
     <div class="btn-group btn-group-sm pull-right">
