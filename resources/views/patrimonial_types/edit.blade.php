dd5077d7f4a7a3293d981da28c863f4de80afc06
eduardo <ecmattos@ig.com.br>
2017-05-24 15:06:11 -0300

    PatrimonialTypes - Cabecalho botao voltar

--- resources/views/patrimonial_types/edit.blade.php
+++ resources/views/patrimonial_types/edit.blade.php
@@ -5,11 +5,9 @@
 	<ol class="breadcrumb">
   		<li class="breadcrumb-item">Administração</li>
   		<li class="breadcrumb-item">Ativos</li>
-  		<li class="breadcrumb-item"><a href="{!! route('patrimonial_types') !!}">Tipos</a></li>
+  		<li class="breadcrumb-item"><a href="{!! route('patrimonial_types') !!}" class="btn btn-sm btn-warning"><i class="fa fa-arrow-left"></i> <b>Tipos</b></a></li>
   		<li class="breadcrumb-item"><b>Alteração</b></li>
 	</ol>
-
-	<hr class="hr-warning" />
 
 	{!! Form::model($patrimonial_type, ['route' => ['patrimonial_types.update', $patrimonial_type->id], 'method' => 'put', 'class' => 'form-horizontal', 'role'=>'form']) !!}
 
