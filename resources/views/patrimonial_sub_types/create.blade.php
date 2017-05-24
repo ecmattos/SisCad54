2870fb8b5c4426f0c1dd80eaa7e34afc61a8fc43
eduardo <ecmattos@ig.com.br>
2017-05-24 15:10:01 -0300

    PatrimonialSubTypes - Cabecalho botao voltar

--- resources/views/patrimonial_sub_types/create.blade.php
+++ resources/views/patrimonial_sub_types/create.blade.php
@@ -5,11 +5,9 @@
 	<ol class="breadcrumb">
   		<li class="breadcrumb-item">Administração</li>
   		<li class="breadcrumb-item">Ativos</li>
-  		<li class="breadcrumb-item"><a href="{!! route('patrimonial_sub_types') !!}">Sub-tipos</a></li>
+  		<li class="breadcrumb-item"><a href="{!! route('patrimonial_sub_types') !!}" class="btn btn-sm btn-warning"><i class="fa fa-arrow-left"></i> <b>Sub-tipos</b></a></li>
   		<li class="breadcrumb-item"><b>Inclusão</b></li>
 	</ol>
-
-	<hr class="hr-warning" />
 
 	{!! Form::open(['route' => 'patrimonial_sub_types.store', 'class'=>'form-horizontal', 'role'=>'form']) !!}
 
