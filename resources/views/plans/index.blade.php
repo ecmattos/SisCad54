588e24fb41c83f74a6df3fa4000e80ca7a908854
eduardo <ecmattos@ig.com.br>
2017-05-24 11:36:57 -0300

    Plans Index - Remocao botao inclusao

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -11,7 +11,7 @@
 		<table class="table table-bordered table-striped" id="table_plans" data-toggle="table" data-toolbar="#filter-bar" data-show-toggle="false" data-search="false" data-show-filter="true" data-show-columns="true" data-show-export="true" data-pagination="true" data-click-to-select="true" data-page-list="[10, 20, 50, 100, All]" data-toolbar="#filter-bar"> 
 			<thead>
 		        <th data-width="1%" class="text-center">
-		        	<a href="{!! route('plans.create') !!}" type="button" class="round round-sm hollow green" rel="tooltip" title="Incluir"><i class="fa fa-file-o"></i></a>
+		        	#
 		        </th>
 		        <th data-width="2%">Código</th>
 		        <th>Descrição</th>
