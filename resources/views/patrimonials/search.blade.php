a9c7176c76d477db9debd41aa302c39c7d91652c
eduardo <ecmattos@ig.com.br>
2017-05-26 18:56:54 -0300

    Patrimonial Search - Cabecalho

--- resources/views/patrimonials/search.blade.php
+++ resources/views/patrimonials/search.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-	
-	<ol class="breadcrumb">
-  		<li class="breadcrumb-item">Patrimônios</li>
-  		<li class="breadcrumb-item"><b>Pesquisa</b></li>
+
+    <ol class="breadcrumb breadcrumb-danger">
+  		<li>PATRIMONIOS</li>
+  		<li><b>Pesquisa</b></li>
 
   		<div class="btn-group btn-group-sm pull-right">
       		<a href="{!! route('patrimonials.create') !!}" type="button" class="round round-sm hollow green" rel="tooltip" title="Incluir"><i class="fa fa-file-o"></i></a>
